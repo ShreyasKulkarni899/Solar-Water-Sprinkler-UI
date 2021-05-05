25d529a48ad9905fb4d79914dd6b5f5fe862e507
Bhargav2310 <bhargavandhe2310@gmail.com>
2021-05-05 16:32:55 +0530

    fixed CropGuideFragment Fragment

--- app/src/main/java/com/nkocet/untitled/CropGuideFragment.java
+++ app/src/main/java/com/nkocet/untitled/CropGuideFragment.java
@@ -1,6 +1,5 @@
 package com.nkocet.untitled;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,14 +10,22 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CropGuideFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_crop_guide, container, false);
+
+        ArrayList<ListModel> crops = new ArrayList<>();
+        crops.add(new ListModel(R.drawable.image_1, "Picture 1"));
+        // Similarly add more objects to list
+        // Or fetch from database
+
         RecyclerView recyclerView = view.findViewById(R.id.lst_recyclerView);
-        ListAdapter adapter = new ListAdapter(getContext(), ListModel.getObjectList());
+        ListAdapter adapter = new ListAdapter(getContext(), crops);
         recyclerView.setAdapter(adapter);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
