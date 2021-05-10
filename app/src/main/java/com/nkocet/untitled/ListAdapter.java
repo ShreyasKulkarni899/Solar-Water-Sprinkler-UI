4175561172b00fc95fb458a590166805e8925519
ShreyasKulkarni899 <sskulkarni899@gmail.com>
2021-05-10 12:02:00 +0530

    shreyasHelpMainScreen2

--- app/src/main/java/com/nkocet/untitled/ListAdapter.java
+++ app/src/main/java/com/nkocet/untitled/ListAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,7 +14,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.card.MaterialCardView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
 
